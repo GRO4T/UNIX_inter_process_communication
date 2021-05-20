f04f9548d3ec7850a608061a90f366ce5f1dcd1e
Damian Kolaska <damian.kolaska99@gmail.com>
2021-05-20 17:54:04 +0200

    Added necessary libraries

---
+++ include/common.hpp
@@ -0,0 +1,13 @@
+#include <loguru.hpp>
+#include <fmt/core.h>
+
+namespace Lindux {
+    inline void loggerInit(
+            int argc, char ** argv, const std::string& logfile = "lindux.log",
+            loguru::Verbosity verbosity = loguru::Verbosity_MAX) {
+        loguru::init(argc, argv);
+        loguru::add_file(logfile.c_str(), loguru::Append, verbosity);
+        // Only show most relevant things on stderr:
+        loguru::g_stderr_verbosity = 1;
+    }
+}
--- test/apps/hello_lindux.cpp
+++ test/apps/hello_lindux.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "dummy.hpp"
+#include "common.hpp"
 
-int main() {
-    Dummy dummy;
+using namespace Lindux;
+
+int main(int argc, char ** argv) {
+    loggerInit(argc, argv); 
+    LOG_S(INFO) << fmt::format("Hello I'm a formatted text {}", 1234);
+    Dummy dummy; 
     dummy.hello();
     return 0;
 }
