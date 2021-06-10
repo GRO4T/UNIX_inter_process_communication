6c310581dc47c7f0d8638d63eec7f8ea8462a9db
Damian Kolaska <damian.kolaska99@gmail.com>
2021-06-10 11:55:26 +0200

    fix connect immediately disconnect test

--- test/integration/client_connect_immediately_disconnect/run.sh
+++ test/integration/client_connect_immediately_disconnect/run.sh
@@ -3,5 +3,5 @@ echo "Test client connect and disconnect"
 rm /dev/shm/sem.bus_mutex && rm /tmp/linda_bus_write && rm /tmp/linda_bus_read
 bin/server > /dev/null 2>&1 &
 sleep 1
-bin/client test.log <<< "exit"
+bin/client test.log < test/integration/client_connect_immediately_disconnect/input.txt
 killall bin/server
