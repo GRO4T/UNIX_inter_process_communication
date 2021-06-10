772074214f397e2458bf9a51d3f868fef172755c
Damian Kolaska <damian.kolaska99@gmail.com>
2021-06-10 11:50:25 +0200

    Run all integration tests

--- test/integration/read_after_input/run.sh
+++ test/integration/read_after_input/run.sh
@@ -4,4 +4,5 @@ echo "Input should return (1, 2). Then read should hang."
 rm /dev/shm/sem.bus_mutex && rm /tmp/linda_bus_write && rm /tmp/linda_bus_read
 bin/server > /dev/null 2>&1 &
 sleep 1
-bin/client test.log < test/integration/read_after_input/input.txt
+bin/client test.log < test/integration/read_after_input/input.txt &
+sleep 2 && echo "TEST: terminating hanging client" && killall bin/client
